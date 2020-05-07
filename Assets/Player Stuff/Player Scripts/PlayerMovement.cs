11e85b69040298c05fea84d92f4b3a34bb35b915
LaksheyHooda <lakshey2016@gmail.com>
2020-05-07 10:35:15 -0700

    Added Movement

--- Assets/Player Stuff/Player Scripts/PlayerMovement.cs
+++ Assets/Player Stuff/Player Scripts/PlayerMovement.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Editable speed and jump force to allow you to perfect player movement.
+    [SerializeField] float playerSpeed = 20;
+    [SerializeField] float playerJumpForce = 20;
 
+    private float previousY = 0;
     // Update is called once per frame
     void Update()
     {
-        
+        //Calls the input handler
+        PlayerInputHandler();
+        previousY = transform.position.y;
+    }
+
+    /// <summary>
+    ///Handles Player Input and chages player velocity acourdingly.
+    /// </summary>
+    private void PlayerInputHandler()
+    {
+        if (Input.GetKey("w"))
+            MovePlayerForward();
+        else if (Input.GetKey("s"))
+            MovePlayerBackward();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            PlayerJump();
+    }
+
+    private void PlayerJump()
+    {
+        if (previousY == transform.position.y)
+        {
+            transform.position += Vector3.up * playerJumpForce * Time.deltaTime;
+        }
+    }
+    private void MovePlayerForward()
+    {
+        transform.position += new Vector3(playerSpeed * Mathf.Sin(transform.eulerAngles.y * (Mathf.PI / 180)) * Time.deltaTime, 0, playerSpeed * Mathf.Cos(transform.eulerAngles.y * (Mathf.PI / 180)) * Time.deltaTime);
+    }
+
+    private void MovePlayerBackward()
+    {
+        transform.position -= new Vector3(playerSpeed * Mathf.Sin(transform.eulerAngles.y * (Mathf.PI / 180)) * Time.deltaTime, 0, playerSpeed * Mathf.Cos(transform.eulerAngles.y * (Mathf.PI / 180)) * Time.deltaTime);
     }
 }
---
+++ Assets/Player Stuff/Player Scripts/StarfLeftAndRight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarfLeftAndRight : MonoBehaviour
+{
+    [SerializeField] float strafSpeed = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlesStarf();
+    }
+
+    private void HandlesStarf()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.position -= new Vector3(strafSpeed * Mathf.Sin((transform.eulerAngles.y + 90) * (Mathf.PI / 180)) * Time.deltaTime, 0, strafSpeed * Mathf.Cos((transform.eulerAngles.y + 90) * (Mathf.PI / 180)) * Time.deltaTime);
+        }
+        else if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(strafSpeed * Mathf.Sin((transform.eulerAngles.y + 90) * (Mathf.PI / 180)) * Time.deltaTime, 0, strafSpeed * Mathf.Cos((transform.eulerAngles.y + 90) * (Mathf.PI / 180)) * Time.deltaTime);
+        }
+    }
+}
